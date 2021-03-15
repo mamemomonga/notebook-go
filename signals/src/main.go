233def756f6b7007c0abc813ad21706e50fee6f2
mamemomonga <mamemomonga@gmail.com>
2021-03-15 15:59:35 +0900

    クラス化

--- signals/src/main.go
+++ signals/src/main.go
@@ -6,13 +6,25 @@ import (
 )
 
 func main() {
-	waitForSignals(loop)
-	log.Println("Finish")
-}
 
-func loop() {
-	for i := 0; i < 30; i++ {
-		log.Printf("%d\n", i)
-		time.Sleep(time.Second)
-	}
+	sil := NewSigintListener()
+
+	go func() {
+		defer func() {
+			log.Println("info: 終了処理をしています")
+			sil.Done()
+		}()
+
+		for i := 0; i < 30; i++ {
+			log.Printf("info: %d/30\n", i)
+			time.Sleep(time.Second)
+			if sil.IsStop() {
+				log.Println("info: 終了処理を受信しました")
+				return
+			}
+		}
+
+	}()
+
+	sil.Start()
 }
--- signals/src/signals.go
+++ signals/src/signals.go
@@ -7,25 +7,46 @@ import (
 	"syscall"
 )
 
-func waitForSignals(mainfunc func()) {
-	sigs := make(chan os.Signal, 1)
-	done := make(chan bool, 1)
+type SigIntListener struct {
+	stopCh chan bool
+	doneCh chan bool
+	sigs   chan os.Signal
+}
 
-	signal.Notify(sigs,
+func NewSigintListener() (t *SigIntListener) {
+	t = new(SigIntListener)
+	t.sigs = make(chan os.Signal, 1)
+	t.doneCh = make(chan bool, 1)
+	t.stopCh = make(chan bool, 1)
+
+	signal.Notify(
+		t.sigs,
 		syscall.SIGKILL,
 		syscall.SIGTERM,
 		syscall.SIGINT,
-		os.Interrupt)
+		os.Interrupt,
+	)
+	return t
+}
 
+func (t *SigIntListener) Start() {
 	go func() {
-		sig := <-sigs
-		log.Printf("Recieved Signal: %s", sig)
-		done <- true
+		sig := <-t.sigs
+		log.Printf("[%s]", sig)
+		t.stopCh <- true
 	}()
+	<-t.doneCh
+}
 
-	go func() {
-		mainfunc()
-		done <- true
-	}()
-	<-done
+func (t *SigIntListener) Done() {
+	t.doneCh <- true
+}
+
+func (t *SigIntListener) IsStop() bool {
+	select {
+	case <-t.stopCh:
+		return true
+	default:
+	}
+	return false
 }
